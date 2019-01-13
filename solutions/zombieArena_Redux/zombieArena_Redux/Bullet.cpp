9e758c574fe1957ce235a7ffdf9130e637e70e87
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2019-01-13 17:54:14 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - 'this' keyword to all mentions of member variables.
    ---
    #### Changes
    - N/A
    ---
    #### Removed
    - N/A

--- solutions/zombieArena_Redux/zombieArena_Redux/Bullet.cpp
+++ solutions/zombieArena_Redux/zombieArena_Redux/Bullet.cpp
@@ -8,15 +8,15 @@
 
 // The constructor
 Bullet::Bullet() {
-	m_BulletShape.setSize(sf::Vector2f(2, 2));
+	this->m_BulletShape.setSize(sf::Vector2f(2, 2));
 }
 
 void Bullet::shoot(float startX, float startY,
 	float targetX, float targetY) {
 	// Keep track of the bullet
-	m_InFlight = true;
-	m_Position.x = startX;
-	m_Position.y = startY;
+	this->m_InFlight = true;
+	this->m_Position.x = startX;
+	this->m_Position.y = startY;
 
 	// Calculate the gradient of the flight path
 	float gradient = (startX - targetX) / (startY - targetY);
@@ -27,64 +27,64 @@ void Bullet::shoot(float startX, float startY,
 	}
 
 	// Calculate the ratio between x and t
-	float ratioXY = m_BulletSpeed / (1 + gradient);
+	float ratioXY = this->m_BulletSpeed / (1 + gradient);
 
 	// Set the "speed" horizontally and vertically
-	m_BulletDistanceY = ratioXY;
-	m_BulletDistanceX = ratioXY * gradient;
+	this->m_BulletDistanceY = ratioXY;
+	this->m_BulletDistanceX = ratioXY * gradient;
 	
 	// Point the bullet in the right direction
 	if (targetX < startX) {
-		m_BulletDistanceX *= -1;
+		this->m_BulletDistanceX *= -1;
 	}
 
 	if (targetY < startY) {
-		m_BulletDistanceY *= -1;
+		this->m_BulletDistanceY *= -1;
 	}
 
 	// Finally, assign the results to the
 	// member variables
-	m_XTarget = targetX;
-	m_YTarget = targetY;
+	this->m_XTarget = targetX;
+	this->m_YTarget = targetY;
 
 	// Set a max range of 1000 pixels
 	float range = 1000;
-	m_MinX = startX - range;
-	m_MaxX = startX + range;
-	m_MinY = startY - range;
-	m_MaxY = startY + range;
+	this->m_MinX = startX - range;
+	this->m_MaxX = startX + range;
+	this->m_MinY = startY - range;
+	this->m_MaxY = startY + range;
 	
 	// Position the bullet ready to be drawn
-	m_BulletShape.setPosition(m_Position);
+	this->m_BulletShape.setPosition(this->m_Position);
 }
 
 void Bullet::stop() {
-	m_InFlight = false;
+	this->m_InFlight = false;
 }
 
 bool Bullet::isInFlight() {
-	return m_InFlight;
+	return this->m_InFlight;
 }
 
 sf::FloatRect Bullet::getPosition() {
-	return m_BulletShape.getGlobalBounds();
+	return this->m_BulletShape.getGlobalBounds();
 }
 
 sf::RectangleShape Bullet::getShape() {
-	return m_BulletShape;
+	return this->m_BulletShape;
 }
 
 void Bullet::update(float elapsedTime) {
 	// Update the bullet position variables
-	m_Position.x += m_BulletDistanceX * elapsedTime;
-	m_Position.y += m_BulletDistanceY * elapsedTime;
+	this->m_Position.x += this->m_BulletDistanceX * elapsedTime;
+	this->m_Position.y += this->m_BulletDistanceY * elapsedTime;
 
 	// Move the bullet
-	m_BulletShape.setPosition(m_Position);
+	this->m_BulletShape.setPosition(this->m_Position);
 
 	// Has the bullet gone out of range?
-	if (m_Position.x < m_MinX || m_Position.x > m_MaxX ||
-		m_Position.y < m_MinY || m_Position.y > m_MaxY)	{
-		m_InFlight = false;
+	if (this->m_Position.x < this->m_MinX || this->m_Position.x > this->m_MaxX ||
+		this->m_Position.y < this->m_MinY || this->m_Position.y > this->m_MaxY)	{
+		this->m_InFlight = false;
 	}
 }
